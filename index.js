8e9d6afb30c74098c0a59e1d994bc8aa5542443a
kujounOfficial <168577284+kujounOfficial@users.noreply.github.com>
2026-04-03 13:39:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("server is running");
+});
+
+app.post("/coords", (req, res) => {
+  const { x, z } = req.body;
+
+  const file = "data.json";
+  let data = [];
+
+  if (fs.existsSync(file)) {
+    data = JSON.parse(fs.readFileSync(file));
+  }
+
+  data.push({ x, z });
+
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
+
+  res.json({ status: "saved" });
 });
 
 const port = process.env.PORT || 3000;
